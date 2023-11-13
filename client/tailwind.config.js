fe22b5884ee5cc49a31763b1c2a2e9a7f883e48d
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2023-11-13 16:06:41 +0800

    /admin/client

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5542F6',
+        highlight: '#EA33F3',
+        header: '#FBFF54',
+        button: '#1D1B4B',
+        edit: '#0FA958',
+        delete: '#F24E1E'
+      },
+    },
+  },
+  plugins: [],
+}
+
