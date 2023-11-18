4a1faa6e131bb7c5843b73b85f71bfb059611f09
neophyte <92195045+kristine-ag@users.noreply.github.com>
2023-11-18 16:42:07 +0800

    Nav.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,41 +2,49 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 
-function Nav({ show }) {
+function Nav({ show, toggleNav }) {
   const inactiveLink = "flex gap-1 p-1";
   const activeLink = inactiveLink + " text-highlight rounded-sm";
 
   const location = useLocation();
 
   return (
-    <aside
-      className={
-        (show ? "left-0" : "-left-full") +
-        " top-0 text-lg text-black font-semibold p-8 fixed w-full bg-white h-full md:static md:w-auto transition-all"
-      }
-    >
-      <div className="mb-4 mr-4">
-        <Logo />
-      </div>
+    <div>
+      {/* Side drawer navigation */}
+      <aside
+        className={`top-0 text-lg text-black font-semibold p-8 fixed w-full bg-white h-full md:static md:w-auto ${
+          show ? "left-0" : "-left-full"
+        } transition-all`}
+      >
+        <div className="mb-4 mr-4">
+          <Logo />
+        </div>
 
-      <nav className="flex flex-col gap-5 items-center text-left">
-        <Link
-          to="/"
-          className={location.pathname === "/" ? activeLink : inactiveLink}
-        >
-          DASHBOARD
-        </Link>
+        {/* Close button inside the drawer when it's open */}
+        {show && (
+          <button onClick={toggleNav} className="md:hidden">
+            Close Navigation
+          </button>
+        )}
 
-        <Link
-          to="/customers"
-          className={
-            location.pathname === "/customers" ? activeLink : inactiveLink
-          }
-        >
-          CUSTOMERS
-        </Link>
+        <nav className="md:flex md:flex-col md:gap-6 md:items-center md:text-center">
+          <Link
+            to="/"
+            className={location.pathname === "/" ? activeLink : inactiveLink}
+          >
+            DASHBOARD
+          </Link>
 
-        <Link
+          <Link
+            to="/customers"
+            className={
+              location.pathname === "/customers" ? activeLink : inactiveLink
+            }
+          >
+            CUSTOMERS
+          </Link>
+
+          <Link
           to="/rewards"
           className={
             location.pathname.startsWith("/rewards") ? activeLink : inactiveLink
@@ -64,7 +72,8 @@ function Nav({ show }) {
               : inactiveLink
           }
         >
-          PURCHASE ORDERS
+          <div>PURCHASE </div>
+          ORDERS
         </Link>
 
         <Link
@@ -87,22 +96,28 @@ function Nav({ show }) {
           CATEGORIES
         </Link>
 
-        <Link
-          to="/orders"
-          className={
-            location.pathname === "/orders" ? activeLink : inactiveLink
-          }
-        >
-          ORDERS
-        </Link>
+          <Link
+            to="/orders"
+            className={
+              location.pathname === "/orders" ? activeLink : inactiveLink
+            }
+          >
+            ORDERS
+          </Link>
 
-        <button
-          className={`${inactiveLink} bg-black text-white text-sm font-normal rounded-md py-3 px-6 text-right whitespace-pre`}
-        >
-          LOGOUT
-        </button>
-      </nav>
-    </aside>
+          <div className="flex flex-col">
+            <div className="flex flex-col flex-grow">
+            </div>
+          </div>
+
+          <button
+            className={`${inactiveLink} bg-black text-white text-sm font-normal rounded-md py-3 px-6 text-right whitespace-pre md:whitespace-normal`}
+          >
+            LOGOUT
+          </button>
+        </nav>
+      </aside>
+    </div>
   );
 }
 
